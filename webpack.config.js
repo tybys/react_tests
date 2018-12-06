07ea16c3c04be4f20b4723fa171ac8194fdebc8c
macbook <macbook@dev.local>
2018-12-06 08:37:04 +0300

    inited

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};

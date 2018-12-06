9a293fff6174f54ad5162b937d96e0375ffd539b
ktabasov <ktabasov@roboxchange.com>
2018-12-06 17:18:47 +0300

    eto pizdec, ebanie configi, nenavijy

--- client/index.js
+++ client/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import { AppContainer } from 'react-hot-loader';
+import ReactDOM  from 'react-dom';
 import AppRouter from './routes';
 
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
+const render = (Component) =>
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('app')
+  );
+
+render(AppRouter);
+if (module.hot) {
+  module.hot.accept('./routes', () => {
+    require('./routes');
+    render(AppRouter);
+  });
+}
---
+++ conf/webpack.development.config.js
@@ -0,0 +1,37 @@
+import webpack from 'webpack';
+import Config from 'webpack-config';
+
+export default new Config().extend('conf/webpack.base.config.js').merge({
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    'react-hot-loader/patch',
+    __dirname + '/../client/index.js'
+  ],
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[local]__[hash:base64:5]",
+              minimize: false
+            }
+          },
+          { loader: 'postcss-loader' },
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+});
